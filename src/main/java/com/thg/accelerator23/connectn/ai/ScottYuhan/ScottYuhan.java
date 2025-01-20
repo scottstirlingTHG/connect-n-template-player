16d12175efb37247a2b2a8e4c258881833136211
Scott Stirling <scott.stirling79@THG.com>
2025-01-20 11:42:34 +0000

    first commit

--- src/main/java/com/thg/accelerator23/connectn/ai/ScottYuhan/ScottYuhan.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ScottYuhan/ScottYuhan.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.ScottYuhan;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class ScottYuhan extends Player {
+  public ScottYuhan(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, ScottYuhan.class.getName());
   }
 
   @Override
